c412402879f08df21a204909b45e31aa4e5a9a77
Edu <edwardmartinse@gmail.com>
2020-03-27 19:23:44 +0100

    work in progress

--- A-star/A-star-algorithm.py
+++ A-star/A-star-algorithm.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
 
     def __init__(self, parent, position):
@@ -8,4 +10,14 @@ class Node:
 
     def __eq__(self, other):
         return self.position == other.position
-        
+
+def calculateEculidianDistance(x,y):
+   return math.sqrt( (x[0]-y[0])**2 + (x[1]-y[1])**2)
+
+
+start = (0,0)
+end = (1,1)
+
+d = calculateEculidianDistance(start,end)
+
+print(d)
--- A-star/utils.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def calculateEculidianDistance(x,y):
-   return math.sqrt( (x[0]-y[0])**2 + (x[1]-y[1])**2)
