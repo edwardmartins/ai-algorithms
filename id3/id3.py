f6d072731936bf8ac031676f639b2d91d9f1ef76
unknown <edumar03@ucm.es>
2020-05-11 04:19:23 +0200

    id3

--- id3/id3.py
+++ id3/id3.py
@@ -1,27 +1,50 @@
 import numpy as np
 from scipy.stats import entropy
 
-def calculate_entropy(data,col):
-    labels, counts = class_counts(data,col)
-    probs = counts/ float(np.shape(data)[0]) # number of rows
+
+def calculate_entropy_dataset(data):
+    labels, counts = class_counts(data, -1)
+    probs = counts / float(np.shape(data)[0])
     entropy = - np.sum(probs * np.log2(probs))
     return entropy
 
+
+"""
+def calculate_entropy_attribute(data,col):
+    target_variables = find_unique_vals(data, -1)
+    variables, counts = class_counts(data,col) # number of times each example appears
+    probs = counts/ float(np.shape(data)[0]) # probability of appareance of each example
+
+    for var in variables: # para cada ejemplo
+        entropy = 0
+        for target in target_variables:
+            numerator = 
+
+"""
+
+
 def read_data():
     with open('data.txt', 'r') as f:
-        data = np.loadtxt('data.txt', dtype=str ,delimiter =',')
+        data = np.loadtxt('data.txt', dtype=str, delimiter=',')
         return data
+
 
 def read_attributes():
     with open('attributes.txt', 'r') as f:
-        attr = np.loadtxt('attributes.txt', dtype=str ,delimiter =',')
+        attr = np.loadtxt('attributes.txt', dtype=str, delimiter=',')
         return attr
 
-def find_unique_vals(data, col):
-    return np.unique(data[:,col]) # row,column
 
-def class_counts(df,col):
-    return np.unique(df[:,col], return_counts=True)
+def find_unique_vals(data, col):
+    return np.unique(data[:, col])  # row,column
+
+
+def class_counts(data, col):
+    return np.unique(data[:, col], return_counts=True)
+
+
+def get_classes(data, col, value):
+    return data[data[:,col] == value][:,-1]
 
 # def id3(attributes, values):
 
@@ -29,7 +52,10 @@ def class_counts(df,col):
 data = read_data()
 print(data)
 print(read_attributes())
-print(calculate_entropy(data,-1))
+print(calculate_entropy_dataset(data))
+print(get_classes(data,0,'lluvioso'))
+print(np.unique(get_classes(data,0,'lluvioso'), return_counts=True))
+
 
 
 """
